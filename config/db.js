0d9d73c75e8367e4015c86f2d5651e8fa2f5967d
akashdeep <wadhwaarjun007@gmail.com>
2026-03-17 13:04:14 +0530

    changed url settings

--- config/db.js
+++ config/db.js
@@ -1,15 +1,18 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_DATABASE,
-    port:process.env.DB_PORT,
-    ssl:{
-        rejectUnauthorized: false
-    }
-});
+// const db = mysql.createConnection({
+//     ur
+//     host:process.env.DB_HOST,
+//     user:process.env.DB_USER,
+//     password:process.env.DB_PASSWORD,
+//     database:process.env.DB_DATABASE,
+//     port:process.env.DB_PORT,
+//     ssl:{
+//         rejectUnauthorized: false
+//     }
+// });
+//
+    const db = mysql.createConnection(process.env.DB_URL);
 
 db.connect(err=>{
 
