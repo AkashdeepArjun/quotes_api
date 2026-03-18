df8da721c6047ae845411dc6e95cc68725a2be97
akashdeep <wadhwaarjun007@gmail.com>
2026-03-18 14:12:15 +0530

    no_api_limit

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const authRoutes = require("./routes/authRoutes")
 const quoteRoutes = require ("./routes/quoteRoutes");
 const express = require("express");
 const cors = require("cors");
+
+const rate_limit = require('express-rate-limit');
+
+
+
+
+
+
+
+
+
 require("dotenv").config();
 
 const app = express();
@@ -11,6 +22,9 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+
+
+
 
 app.use("/api/auth",authRoutes);
 
@@ -25,4 +39,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>{
     console.log(`SERVER RUNNNING ON PORT ${PORT}`);
 });
+
+
 
