ec2c804728390a769633d874836dde8428916522
akashdeep <wadhwaarjun007@gmail.com>
2026-03-18 14:30:17 +0530

    rate limit pro max

--- config/db.js
+++ config/db.js
@@ -1,17 +1,6 @@
 const mysql = require("mysql2");
 
-// const db = mysql.createConnection({
-//     ur
-//     host:process.env.DB_HOST,
-//     user:process.env.DB_USER,
-//     password:process.env.DB_PASSWORD,
-//     database:process.env.DB_DATABASE,
-//     port:process.env.DB_PORT,
-//     ssl:{
-//         rejectUnauthorized: false
-//     }
-// });
-//
+
     const db = mysql.createConnection(process.env.DB_URL);
 
 db.connect(err=>{
